5d04d46d9d31acb30d97de463c82617af08c9418
Alexey Chekhotskiy <scubacube@outlook.com>
2017-02-08 18:10:47 +0100

    в

--- js/common.js
+++ js/common.js
@@ -27,13 +27,60 @@ $(".arrow-bottom").click(function() {
 		return false;
 	});
 
-// $('.item-img').hover(function() {
-//     /*fade in code*/
-// $('.item-text-description').removeClass('hidden');
-// }, function() {
-//     /*fade out code*/
-//     $('.item-text-description').addClass('hidden');
-// });
+$('.item-img').hover(function() {
+    /*fade in code*/
+$('.item-text-description').removeClass('hidden');
+}, function() {
+    /*fade out code*/
+    $('.item-text-description').addClass('hidden');
+});
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows'
+});
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
 
+
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+
+
+
+$('a[href*="#"]:not([href="#"])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	var target = $(this.hash);
+	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	if (target.length) {
+	$('html, body').animate({
+		scrollTop: target.offset().top
+	}, 1000);
+	return false;
+	}
+}
+});
 
 });
