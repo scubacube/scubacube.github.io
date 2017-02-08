788410b4464f85adfa683fb88fde6cbb4269ddcb
Alexey Chekhotskiy <scubacube@outlook.com>
2017-02-08 21:06:26 +0100

    gh

--- js/common.js
+++ js/common.js
@@ -1,12 +1,12 @@
 $(function() {
-// $('.item-text-description').equalHeight();
+$('.item-text-description p').equalHeight();
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
-
+	  
 // function wResize() {
 // 		$("header").css("min-height", $(window).height());
 // 	};
