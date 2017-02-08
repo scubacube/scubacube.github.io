88d9f238d6333cdc38747758949b98fecf1d9ca0
Alexey Chekhotskiy <scubacube@outlook.com>
2017-02-08 13:24:19 +0100

    f

--- demos/real-estate/js/common.js
+++ demos/real-estate/js/common.js
@@ -6,3 +6,21 @@ $(".toggle-mnu").click(function() {
 	$(".main-mnu").slideToggle();
 	return false;
 });
+
+ $('.slider-main').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	fade: true,
+	asNavFor: '.slider-nav',
+	nextArrow: '<i class="fa fa-chevron-right"></i>',
+	prevArrow: '<i class="fa fa-chevron-left"></i>'
+});
+$('.slider-nav').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	asNavFor: '.slider-main',
+	infinite: true,
+	focusOnSelect: true,
+	nextArrow: '<i class="fa fa-chevron-right"></i>',
+	prevArrow: '<i class="fa fa-chevron-left"></i>'
+});
